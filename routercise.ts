64747b7583e2253dede23444ccd9975e1337b039
Blake Ramsdell <blaker@gmail.com>
2019-06-30 16:12:31 -0700

    Add canvas and some pads

--- routercise.ts
+++ routercise.ts
@@ -1,1 +1,26 @@
-document.body.textContent = "Routercise is good for you"
+class Pad {
+    id : string;
+    x : number;
+    y : number;
+    diameter : number;
+}
+
+let pads: Pad[] = [
+    {id: 'A1', x: 10, y: 10, diameter: 2.54},
+    {id: 'A2', x: 10, y: 20, diameter: 2.54},
+    {id: 'B1', x: 20, y: 10, diameter: 2.54},
+    {id: 'B2', x: 20, y: 20, diameter: 2.54},
+];
+
+const canvas = <HTMLCanvasElement>document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+ctx.fillStyle = 'green';
+
+// Make things bigger so we can see them
+ctx.scale(5, 5);
+
+// Draw all the pads
+for (let pad of pads) {
+    ctx.fillRect(pad.x, pad.y, pad.diameter, pad.diameter);
+}
