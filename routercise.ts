18105864e3008af97c75ae7869060f7999c27ed3
Blake Ramsdell <blaker@gmail.com>
2019-06-30 19:57:39 -0700

    Add trace drawing

--- routercise.ts
+++ routercise.ts
@@ -15,36 +15,48 @@ let pads: Pad[] = [
 const svg = <SVGSVGElement> <any> document.getElementById("svg");
 const parentG = <SVGGElement> svg.getElementsByTagName("g")[0];
 
+var traceWidth = 0.5;
+
 // Draw all the pads
 for (let pad of pads) {
-    let circle = <SVGCircleElement> document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    let circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
     circle.setAttribute("cx", String(pad.x));
     circle.setAttribute("cy", String(pad.y));
     circle.setAttribute("r", String(pad.diameter / 2));
     parentG.appendChild(circle);
 }
 
-var mouseStart = undefined;
+function getCoordinates(e: MouseEvent) {
+    let coord = svg.createSVGPoint();
+    coord.x = e.x;
+    coord.y = e.y;
+    return coord.matrixTransform(parentG.getScreenCTM().inverse());
+}
+
+var currentLine: SVGLineElement;
 
 // Hook the mouse events to draw wires
 svg.onmousedown = function(e) {
-    mouseStart = svg.createSVGPoint();
-    mouseStart.x = e.x;
-    mouseStart.y = e.y;
-    mouseStart = mouseStart.matrixTransform(parentG.getScreenCTM().inverse());
-    console.log("mousedown", mouseStart.x, mouseStart.y);
+    const mouseStart = getCoordinates(e);
+    currentLine = document.createElementNS("http://www.w3.org/2000/svg", "line");
+    currentLine.setAttribute("x1", String(mouseStart.x));
+    currentLine.setAttribute("y1", String(mouseStart.y));
+    currentLine.setAttribute("x2", String(mouseStart.x));
+    currentLine.setAttribute("y2", String(mouseStart.y));
+    currentLine.setAttribute("stroke", "black");
+    currentLine.setAttribute("stroke-linecap", "round");
+    currentLine.setAttribute("stroke-width", String(traceWidth));
+    parentG.appendChild(currentLine);
 }
 
 svg.onmousemove = function(e) {
-    if (mouseStart) {
-        let mouseEnd = svg.createSVGPoint();
-        mouseEnd.x = e.x;
-        mouseEnd.y = e.y;
-        mouseEnd = mouseEnd.matrixTransform(parentG.getScreenCTM().inverse());
-        console.log("mousedown", mouseEnd.x, mouseEnd.y);
-    }
+    if (currentLine) {
+        let mouseEnd = getCoordinates(e);
+        currentLine.setAttribute("x2", String(mouseEnd.x));
+        currentLine.setAttribute("y2", String(mouseEnd.y));
+        }
 }
 
 svg.onmouseup = function(e) {
-    mouseStart = undefined;
+    currentLine = undefined;
 }
